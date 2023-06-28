4d979c61d4477656e4e4741148857f0221fb2a22
Faranosh Amini <faranosh.amini4@gmail.com>
2023-06-28 10:39:31 +0430

    Fix rubocop errors

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,55 +1,55 @@
 class FoodsController < ApplicationController
-    before_action :set_food, only: %i[show destroy]
-    before_action :set_user, only: %i[index show create]
-  
-    def index
-      # @user = User.find(params[:user_id])
-      @foods = @user.foods.sort.reverse
-      render :index
-    end
-  
-    def new
-      @food = Food.new
+  before_action :set_food, only: %i[show destroy]
+  before_action :set_user, only: %i[index show create]
+
+  def index
+    # @user = User.find(params[:user_id])
+    @foods = @user.foods.sort.reverse
+    render :index
+  end
+
+  def new
+    @food = Food.new
+    render :new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @food = @user.foods.new(**food_params)
+    if @food.save
+      flash[:success] = 'Food saved successfully'
+      redirect_to user_foods_url
+    else
+      flash.now[:error] = 'Error: Food could not be saved'
       render :new
     end
-  
-    def create
-      @user = User.find(params[:user_id])
-      @food = @user.foods.new(**food_params)
-      if @food.save
-        flash[:success] = 'Food saved successfully'
-        redirect_to user_foods_url
-      else
-        flash.now[:error] = 'Error: Food could not be saved'
-        render :new
-      end
-    end
-  
-    def destroy
-      @user = current_user
-      if @food.destroy
-        flash[:success] = 'Food was successfully deleted.'
-      else
-        flash[:error] = 'Error: Food could not be deleted'
-      end
-      redirect_to user_foods_url
-    end
-  
-    def show
-      @food
-    end
-  
-    private
-  
-    def set_user
-      @user = User.find(params[:user_id])
-    end
-  
-    def set_food
-      @food = set_user.foods.find(params[:id])
-    end
-  
-    def food_params
-      params.require(:food).permit(:name, :measurement_unit, :price, :quantity, :user_id)
-    end
   end
+
+  def destroy
+    @user = current_user
+    if @food.destroy
+      flash[:success] = 'Food was successfully deleted.'
+    else
+      flash[:error] = 'Error: Food could not be deleted'
+    end
+    redirect_to user_foods_url
+  end
+
+  def show
+    @food
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def set_food
+    @food = set_user.foods.find(params[:id])
+  end
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity, :user_id)
+  end
+end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,41 +1,41 @@
 class RecipesController < ApplicationController
-    before_action :recipe_params, only: :create
-    def new
-      @recipe = Recipe.new
-    end
-  
-    def create
-      @user = current_user
-      @recipe = @user.recipes.new(**recipe_params)
-      if @recipe.save
-        redirect_to user_recipes_path, notice: 'recipe was successfully created'
-      else
-        render :new, alert: "Couldn't create recipe for user"
-      end
-    end
-  
-    def index
-      @user = current_user
-      @recipes = Recipe.all
-    end
-  
-    def show
-      @user = current_user
-      @recipe = Recipe.find(params[:id])
-      @foods = Food.all
-      @recipe_foods = @recipe.recipe_foods.includes(:food, :recipe).order(:id)
-    end
-  
-    def destroy
-      @user = current_user
-      @recipe = @user.recipes.find(params[:id])
-      @recipe.destroy!
-      redirect_to user_recipes_path
-    end
-  
-    private
-  
-    def recipe_params
-      params.require(:recipe).permit(:name, :preperation_time, :cooking_time, :description, :public, :user_id)
+  before_action :recipe_params, only: :create
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @user = current_user
+    @recipe = @user.recipes.new(**recipe_params)
+    if @recipe.save
+      redirect_to user_recipes_path, notice: 'recipe was successfully created'
+    else
+      render :new, alert: "Couldn't create recipe for user"
     end
   end
+
+  def index
+    @user = current_user
+    @recipes = Recipe.all
+  end
+
+  def show
+    @user = current_user
+    @recipe = Recipe.find(params[:id])
+    @foods = Food.all
+    @recipe_foods = @recipe.recipe_foods.includes(:food, :recipe).order(:id)
+  end
+
+  def destroy
+    @user = current_user
+    @recipe = @user.recipes.find(params[:id])
+    @recipe.destroy!
+    redirect_to user_recipes_path
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preperation_time, :cooking_time, :description, :public, :user_id)
+  end
+end
