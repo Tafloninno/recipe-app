ba100a658e4d869e01b2a3413d45f71442318fec
Mugisha Innocent <111200646+Tafloninno@users.noreply.github.com>
2023-06-29 10:41:15 +0300

    Add shopping list controller

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,8 +1,15 @@
 class ShoppingListsController < ApplicationController
   before_action :authenticate_user!, except: [:show]
+
   def index
     @current_user = current_user
-    @foods = current_user.recipes.includes!(:foods).map(&:foods).flatten.uniq
-    @total_price = @foods.map { |ingredient| ingredient.price * ingredient.total_quantity_recipes }.sum
+    @foods = current_user.recipes.includes(foods: :recipe_foods).map(&:foods).flatten.uniq
+    @total_price = calculate_total_price(@foods)
+  end
+
+  private
+
+  def calculate_total_price(foods)
+    foods.sum { |food| food.price * food.total_quantity_recipes }
   end
 end
