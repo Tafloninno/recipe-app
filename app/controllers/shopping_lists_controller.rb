3160ca4ad1d148415609747bade40b8d3fd78b2e
Mugisha Innocent <111200646+Tafloninno@users.noreply.github.com>
2023-06-29 10:45:55 +0300

    Add shoppist list controller

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -5,11 +5,5 @@ class ShoppingListsController < ApplicationController
     @current_user = current_user
     @foods = current_user.recipes.includes(foods: :recipe_foods).map(&:foods).flatten.uniq
     @total_price = calculate_total_price(@foods)
-  end
-
-  private
-
-  def calculate_total_price(foods)
-    foods.sum { |food| food.price * food.total_quantity_recipes }
-  end
+  end 
 end
