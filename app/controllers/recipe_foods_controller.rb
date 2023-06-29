0e521d02cf60fe779912aea89df895247e1c02fb
Faranosh Amini <faranosh.amini4@gmail.com>
2023-06-29 05:24:41 +0430

    Update recipe_foods_controller.rb

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,30 +1,48 @@
 class RecipeFoodsController < ApplicationController
   def new
     @recipe_food = RecipeFood.new
+    @recipe = Recipe.find(params[:recipe_id])
+    @remaining_foods = remaining_foods
   end
 
   def create
-    @recipe_food = RecipeFood.new(recipe_food_params)
-    @recipe_food.recipe_id = params[:recipe_id]
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_food = RecipeFood.new(recipe_id: @recipe.id, food_id: recipe_food_params[:food_id],
+                                  quantity: recipe_food_params[:quantity])
     if @recipe_food.save
-      flash[:success] = 'Recipe Food succesfully added'
-      redirect_to recipe_foods_path
+      flash[:notice] = 'Food added successfully !'
+      redirect_to recipe_path(@recipe)
     else
-      flash[:error] = 'Error: Recipe Food could not be added'
-      render :new
+      flash[:notice] = 'Invalid Entry'
+      redirect_to current_path
     end
   end
 
   def destroy
-    @recipe_food = RecipeFood.find(params[:id])
-    @recipe_food.destroy
-    flash[:notice] = 'You deleted Recipe food successfully'
-    redirect_back(fallback_location: root_path)
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_food = @recipe.recipe_foods.all.find(params[:id])
+    flash[:notice] = if @recipe_food.destroy
+                       'Food Removed Successfully '
+                     else
+                       'Invalid Transaction'
+                     end
+    redirect_to recipe_path(@recipe)
   end
 
   private
 
   def recipe_food_params
-    params.require(:recipe_food).permit(:quantity, :food_id)
+    params.require(:recipe_food).permit(:food_id, :quantity)
+  end
+
+  def remaining_foods
+    total_foods = current_user.foods
+    total_recipe_foods = []
+    current_user.recipes.includes(:recipe_foods).each do |recipe|
+      recipe.recipe_foods.includes(:food).each do |recipe_food|
+        total_recipe_foods << recipe_food.food
+      end
+    end
+    total_foods - total_recipe_foods
   end
 end
